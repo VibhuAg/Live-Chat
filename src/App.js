e879d9a7574bd25860e97ee1a2fc414f0679a8db
Vibhu Agrawal <vibhu1.agrawal@gmail.com>
2021-09-17 23:54:28 -0400

    fix typo

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import logo from "./logo.svg";
 import "./App.css";
-import CreateRoom from "./Pages/CreateRooms";
+import CreateRoom from "./Pages/CreateRoom";
 
 function App() {
   return <CreateRoom />;
